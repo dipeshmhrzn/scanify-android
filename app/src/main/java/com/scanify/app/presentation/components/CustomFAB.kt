73b8ae4caf23c79fd0ebb6212364a093286634c1
dipeshmhrzn <dipeshmhrzn28@gmail.com>
2026-05-20 22:53:11 +0545

    chore: add app icon assets

--- app/src/main/java/com/scanify/app/presentation/components/CustomFAB.kt
+++ app/src/main/java/com/scanify/app/presentation/components/CustomFAB.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.scanify.app.ui.theme.BrandGradient
 
-@Preview(showBackground = true)
 @Composable
 fun CustomFAB() {
 
